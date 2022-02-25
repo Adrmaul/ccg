9f639d447463b32909fae7690de9e08a46871128
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-02-25 20:22:30 +0700

    Create config.py

---
+++ amime/config.py
@@ -0,0 +1,19 @@
+from typing import Dict, List
+
+
+GROUPS: Dict = {
+    "en": -1001559315851,
+    "pt": -1001559315851,
+}
+CHANNELS: Dict = {
+    "en": -1001706431524,
+    "pt": -1001706431524,
+}
+CHATS: Dict = {
+    "backup": -1001559315851,
+    "requests": -1001559315851,
+    "videos": -1001559315851,
+    "staff": -1001559315851,
+}
+SUDO_USERS: List[int] = [1765145725, 918317361, 705707638]  # @AndrielFR, @Hitalo and @VegaZS
+PREFIXES: List[str] = ["!", "."] # Like !start or /start
