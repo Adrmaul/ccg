6fb2318a5b56f37660ab86dccf5134d1e2a7653a
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-02-25 23:30:30 +0700

    tes

--- amime/modules/requests.py
+++ amime/modules/requests.py
@@ -128,7 +128,15 @@ async def request_done(bot: Amime, callback: CallbackQuery):
                     type=content_type.upper(),
                 ),
                 reply_markup=ikb(
-                    [[(lang.view_more_button, f"{content_type} {content_id}")]]
+                    keyboard = [
+                [
+                    (
+                        lang.view_more_button,
+                        f"https://t.me/{bot.me.username}/?start=anime_{anime.id}",
+                        "url",
+                    )
+                ],
+            ]
                 ),
             )
         except BaseException:
