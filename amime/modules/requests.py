620152f4dd657b8c86452dd27a7a9096bb84181d
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-02-25 23:48:21 +0700

    tes

--- amime/modules/requests.py
+++ amime/modules/requests.py
@@ -41,7 +41,7 @@ async def request_get(bot: Amime, callback: CallbackQuery):
 
     text_splited = message.text.html.splitlines()
     text = "\n".join(text_splited[: len(text_splited) - 2])
-    text += f"\n<b>Diunggah oleh</b>: {user.784985038()}"
+    text += f"\n<b>Diunggah oleh</b>: {784985038}"
     text += "\n\n#REQUEST"
 
     await message.edit_text(
