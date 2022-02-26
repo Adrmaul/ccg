4110b395e549a8b1e044578cdec6133bd924b174
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-02-26 08:17:22 +0700

    tes

--- amime/modules/anime/upcoming.py
+++ amime/modules/anime/upcoming.py
@@ -44,7 +44,7 @@ async def anime_upcoming(bot: Amime, callback: CallbackQuery):
             json=dict(
                 query="""
                 query($per_page: Int) {
-                    Page(page: 1, perPage: $per_page) {
+                    Page(page: 2, perPage: $per_page) {
                         media(type: ANIME, sort: POPULARITY_DESC, status: NOT_YET_RELEASED) {
                             id
                             title {
