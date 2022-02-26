5ca5764ab784d2f26154ca511f6255781d1f2cd0
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-02-26 18:59:12 +0700

    tes

--- amime/modules/requests.py
+++ amime/modules/requests.py
@@ -128,7 +128,7 @@ async def request_done(bot: Amime, callback: CallbackQuery):
                     type=content_type.upper(),
                 ),
                 reply_markup=ikb(
-                    [[(lang.view_more_button, f"{content_type} {content_id}")]]
+                    [[(lang.checknimex_button_button, f"{content_type} {content_id}")]]
                 ),
                 
             )
