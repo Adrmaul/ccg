cbf58eb0d3f7834e28b833adfa761a95d4dfc5f6
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-02-26 16:33:57 +0700

    rw

--- amime/modules/requests.py
+++ amime/modules/requests.py
@@ -41,8 +41,8 @@ async def request_get(bot: Amime, callback: CallbackQuery):
 
     text_splited = message.text.html.splitlines()
     text = "\n".join(text_splited[: len(text_splited) - 2])
-    text += f"<b>Diproses oleh</b>: {user.mention()}"
-    text += "\n#REQUEST"
+    text += f"\n<b>Diproses oleh</b>: {user.mention()}"
+    text += "\n\n#REQUEST"
 
     await message.edit_text(
         text,
