6cb35ee8f9a717253e8d7596d0f3ccf709b44ca1
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-02-28 20:28:23 +0700

    tes

--- amime/modules/anime/suggestions.py
+++ amime/modules/anime/suggestions.py
@@ -43,7 +43,7 @@ async def anime_suggestions(bot: Amime, callback: CallbackQuery):
             url="https://graphql.anilist.co",
             json=dict(
                 query="""
-                query($page: Int, $per_page: Int) {
+                query($page: Int, $perPage: Int) {
                     Page(page: $page, perPage: $per_page) {
                         media(type: ANIME, sort: TRENDING_DESC) {
                             id
