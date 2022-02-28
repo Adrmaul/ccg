8dc8a7d6c5b3d6de00b6b927183b0895ca722fd3
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-02-28 11:41:24 +0700

    tes

--- amime/modules/anime/view.py
+++ amime/modules/anime/view.py
@@ -178,9 +178,9 @@ async def anime_view(bot: Amime, union: Union[CallbackQuery, Message]):
                         lang.serupa_button,
                         f"https://t.me/{bot.me.username}/?start=anirec_{anime.id}",
                         "url",
-                    )
-                )    
-        buttons.append(lang.search_button, switch_inline_query_current_chat={anime.title.romaji})      
+                    ),
+                    (lang.search_button, "", "switch_inline_query_current_chat=name"),
+                )      
 
         if is_private and not anime.status.lower() == "not_yet_released":
             button = (
