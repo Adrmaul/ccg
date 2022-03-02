41275b179bbd4c3bb522f3d7b9c8aa1e7e02aa6b
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-03-02 13:31:54 +0700

    Update movie.py

--- amime/modules/anime/movie.py
+++ amime/modules/anime/movie.py
@@ -23,7 +23,7 @@ async def anime_suggestions(bot: Amime, callback: CallbackQuery):
                 query="""
                 query($page: Int, $perPage: Int) {
                     Page(page: $page, perPage: $perPage) {
-                        media(type: ANIME, format: MOVIE, sort: TRENDING_DESC, genre: Action) {
+                        media(type: ANIME, format: MOVIE, sort: TRENDING_DESC, genre: ['action']) {
                             id
                             title {
                                 romaji
