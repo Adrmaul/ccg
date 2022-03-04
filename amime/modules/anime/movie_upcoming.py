da0cc263dd7f1dfc6b5bd76ea98c77e55cf7570a
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-03-04 15:10:04 +0700

    fixed

--- amime/modules/anime/movie_upcoming.py
+++ amime/modules/anime/movie_upcoming.py
@@ -55,7 +55,7 @@ async def anime_suggestions(bot: Amime, callback: CallbackQuery):
 
             layout = Pagination(
                 suggestions,
-                item_data=lambda i, pg: f"https://t.me/ccgnimex_bot/?start=anime_{i.id}", 
+                item_data=lambda i, pg: f"menu {i.id}",
                 item_title=lambda i, pg: i.title.romaji,
                 page_data=lambda pg: f"upcoming_movie anime {pg}",
             )
