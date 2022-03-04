11de4c74796388f968e8dc6a7393e9a311b31bfe
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-03-04 13:18:58 +0700

    tes

--- amime/modules/anime/movie_top/movie_top2.py
+++ amime/modules/anime/movie_top/movie_top2.py
@@ -8,7 +8,7 @@ from pyromod.nav import Pagination
 from amime.amime import Amime
 
 
-@Amime.on_callback_query(filters.regex(r"^top_movie1 anime (?P<page>\d+)"))
+@Amime.on_callback_query(filters.regex(r"^top_movie2 anime (?P<page>\d+)"))
 async def anime_suggestions(bot: Amime, callback: CallbackQuery):
     page = int(callback.matches[0]["page"])
 
@@ -57,7 +57,7 @@ async def anime_suggestions(bot: Amime, callback: CallbackQuery):
                 suggestions,
                 item_data=lambda i, pg: f"menu {i.id}",
                 item_title=lambda i, pg: i.title.romaji,
-                page_data=lambda pg: f"top_movie1 anime {pg}",
+                page_data=lambda pg: f"top_movie2 anime {pg}",
             )
 
             lines = layout.create(page, lines=8)
