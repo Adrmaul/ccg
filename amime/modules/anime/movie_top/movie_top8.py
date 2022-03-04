2102e45b71be914913644234b7173f36f90b6f3d
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-03-04 14:00:39 +0700

    tes

--- amime/modules/anime/movie_top/movie_top8.py
+++ amime/modules/anime/movie_top/movie_top8.py
@@ -64,7 +64,7 @@ async def anime_suggestions(bot: Amime, callback: CallbackQuery):
 
             if len(lines) > 0:
                 keyboard += lines
-    keyboard.append([(lang.Prev, "top_movie7 anime 1"), (lang.Next, "top_movie9 anime ")])
+    keyboard.append([(lang.Prev, "top_movie7 anime 1"), (lang.Next, "top_movie9 anime 1")])
     keyboard.append([(lang.back_button, "movie-menu")])
 
     await message.edit_text(
