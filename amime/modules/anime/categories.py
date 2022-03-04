04493d016912a4a3198e261b0babf5f1fe002e69
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-03-04 11:05:26 +0700

    tes

--- amime/modules/anime/categories.py
+++ amime/modules/anime/categories.py
@@ -106,3 +106,68 @@ async def anime_categorie(bot: Amime, callback: CallbackQuery):
         lang.categorie_text.format(genre=lang.strings[lang.code][categorie]),
         reply_markup=ikb(keyboard),
     )
+#menu genre:
+
+@Amime.on_callback_query(filters.regex(r"^categorie anime action (?P<page>\d+)"))
+async def anime_suggestions(bot: Amime, callback: CallbackQuery):
+    page = int(callback.matches[0]["page"])
+
+    message = callback.message
+    lang = callback._lang
+
+    keyboard = []
+    async with httpx.AsyncClient(http2=True) as client:
+        response = await client.post(
+            url="https://graphql.anilist.co",
+            json=dict(
+                query="""
+                query($page: Int) {
+                    Page (perPage: 100, page: $page) {
+                        media(type: ANIME, format: MOVIE, sort: TRENDING_DESC) {
+                            id
+                            title {
+                                romaji
+                                english
+                                native
+                            }
+                            siteUrl
+                        }
+                    }
+                }
+                """,
+                variables=dict(
+                    perPage=100,
+                ),
+            ),
+            headers={
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+        )
+        data = response.json()
+        await client.aclose()
+        if data["data"]:
+            items = data["data"]["Page"]["media"]
+            suggestions = [
+                Anime(id=item["id"], title=item["title"], url=item["siteUrl"])
+                for item in items
+            ]
+
+            layout = Pagination(
+                suggestions,
+                item_data=lambda i, pg: f"categories anime {i.id}",
+                item_title=lambda i, pg: i.title.romaji,
+                page_data=lambda pg: f"categorie anime action {pg}",
+            )
+
+            lines = layout.create(page, lines=8)
+
+            if len(lines) > 0:
+                keyboard += lines
+
+    keyboard.append([(lang.back_button, "categories anime 1")])
+
+    await message.edit_text(
+        lang.suggestions_text,
+        reply_markup=ikb(keyboard),
+    )
