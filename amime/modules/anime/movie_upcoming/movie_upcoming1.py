13474a85b038f3fca4b061d9b0e04776229a8ff4
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-03-04 15:28:07 +0700

    tes

--- amime/modules/anime/movie_upcoming/movie_upcoming1.py
+++ amime/modules/anime/movie_upcoming/movie_upcoming1.py
@@ -8,7 +8,7 @@ from pyromod.nav import Pagination
 from amime.amime import Amime
 
 
-@Amime.on_callback_query(filters.regex(r"^upcoming_movie anime (?P<page>\d+)"))
+@Amime.on_callback_query(filters.regex(r"^upcoming_movie1 anime (?P<page>\d+)"))
 async def anime_suggestions(bot: Amime, callback: CallbackQuery):
     page = int(callback.matches[0]["page"])
 
@@ -21,8 +21,8 @@ async def anime_suggestions(bot: Amime, callback: CallbackQuery):
             url="https://graphql.anilist.co",
             json=dict(
                 query="""
-                query($page: Int) {
-                    Page (perPage: 100, page: $page) {
+                query($per_page: Int) {
+                    Page(page: 2, perPage: $per_page) {
                         media(type: ANIME, format: MOVIE, sort: POPULARITY_DESC, status: NOT_YET_RELEASED) {
                             id
                             title {
@@ -57,14 +57,14 @@ async def anime_suggestions(bot: Amime, callback: CallbackQuery):
                 suggestions,
                 item_data=lambda i, pg: f"menu {i.id}",
                 item_title=lambda i, pg: i.title.romaji,
-                page_data=lambda pg: f"upcoming_movie anime {pg}",
+                page_data=lambda pg: f"upcoming_movie1 anime {pg}",
             )
 
             lines = layout.create(page, lines=8)
 
             if len(lines) > 0:
                 keyboard += lines
-
+    keyboard.append([(lang.Prev, "upcoming_movie anime 1"), (lang.Next, "upcoming_movie2 anime 1")])
     keyboard.append([(lang.back_button, "movie-menu")])
 
     await message.edit_text(
