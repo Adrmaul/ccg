d0a6fa695f7064447abb5f594194a16cebfac3ed
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-03-05 09:44:37 +0700

    tes

--- amime/modules/anime/TV-SHORT/tvshort_trend/tvshort_trend3.py
+++ amime/modules/anime/TV-SHORT/tvshort_trend/tvshort_trend3.py
@@ -8,7 +8,7 @@ from pyromod.nav import Pagination
 from amime.amime import Amime
 
 
-@Amime.on_callback_query(filters.regex(r"^tvshow_trending3 anime (?P<page>\d+)"))
+@Amime.on_callback_query(filters.regex(r"^tvshort_trending3 anime (?P<page>\d+)"))
 async def anime_suggestions(bot: Amime, callback: CallbackQuery):
     page = int(callback.matches[0]["page"])
 
@@ -23,7 +23,7 @@ async def anime_suggestions(bot: Amime, callback: CallbackQuery):
                 query="""
                 query($per_page: Int) {
                     Page(page: 4, perPage: $per_page) {
-                        media(type: ANIME, format: TV, sort: TRENDING_DESC, status: FINISHED) {
+                        media(type: ANIME, format: TV_SHORT, sort: TRENDING_DESC, status: FINISHED) {
                             id
                             title {
                                 romaji
@@ -57,15 +57,15 @@ async def anime_suggestions(bot: Amime, callback: CallbackQuery):
                 suggestions,
                 item_data=lambda i, pg: f"menu {i.id}",
                 item_title=lambda i, pg: i.title.romaji,
-                page_data=lambda pg: f"tvshow_trending3 anime {pg}",
+                page_data=lambda pg: f"tvshort_trending3 anime {pg}",
             )
 
             lines = layout.create(page, lines=8)
 
             if len(lines) > 0:
                 keyboard += lines
-    keyboard.append([(lang.Prev, "tvshow_trending2 anime 1"), (lang.Next, "tvshow_trending4 anime 1")])
-    keyboard.append([(lang.back_button, "tvshow_menu")])
+    keyboard.append([(lang.Prev, "tvshort_trending2 anime 1"), (lang.Next, "tvshort_trending4 anime 1")])
+    keyboard.append([(lang.back_button, "tvshort_menu")])
 
     await message.edit_text(
         lang.suggestions_text,
