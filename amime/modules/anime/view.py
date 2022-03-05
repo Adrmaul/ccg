61457f748293b6ba386e019371b428b80c4bb43c
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-03-05 09:57:22 +0700

    ts

--- amime/modules/anime/view.py
+++ amime/modules/anime/view.py
@@ -160,7 +160,7 @@ async def anime_view(bot: Amime, union: Union[CallbackQuery, Message]):
         if is_private and not anime.status.lower() == "not_yet_released":
             buttons.append(
                 (
-                    lang.manage_button,
+                    lang.batch_button,
                     f"https://t.me/downloadanimebatch/302",
                         "url",
                 )
