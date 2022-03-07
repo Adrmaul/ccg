3c3f74ef9f3c040aadeb715bf807e987b8ffcc5e
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-03-07 11:02:23 +0700

    Update ktgr_menu-movie.py

--- amime/modules/anime/MOVIE/KATEGORI/ktgr_menu-movie.py
+++ amime/modules/anime/MOVIE/KATEGORI/ktgr_menu-movie.py
@@ -17,12 +17,44 @@ async def anime_menu(bot: Amime, union: Union[CallbackQuery, Message]):
     keyboard = [
         [
             (lang.ACTION, "movie_action anime 1"),
+            (lang.ADVENTURE, "movie_adventure anime 1"),
         ],
-        
+        [
+            (lang.COMEDY, "movie_comedy anime 1"),
+            (lang.DRAMA, "movie_drama anime 1"),
+        ],
+        [
+            (lang.ECCHI, "movie_ecchi anime 1"),
+            (lang.FANTASY, "movie_fantasy anime 1"),
+        ],
+        [
+            (lang.HORROR, "movie_horror anime 1"),
+            (lang.MAHOUSJ, "movie_mahousj anime 1"),
+        ],
+        [
+            (lang.MECHA, "movie_mecha anime 1"),
+            (lang.MUSIC, "movie_music anime 1"),
+        ],
+        [
+            (lang.MYSTERY, "movie_mystery anime 1"),
+            (lang.PSYCHOLOGICAL, "movie_psychological anime 1"),
+        ],
+        [
+            (lang.ROMANCE, "movie_romance anime 1"),
+            (lang.SCIFI, "movie_scifi anime 1"),
+        ],
+        [
+            (lang.SOL, "movie_sol anime 1"),
+            (lang.SPORTS, "movie_sports anime 1"),
+        ],
+        [
+            (lang.SUPERNATURAL, "movie_supernatural anime 1"),
+            (lang.THRILLER, "movie_trhriller anime 1"),
+        ],
     ]
 
     if is_callback:
-        keyboard.append([(lang.back_button, "menu")])
+        keyboard.append([(lang.back_button, "movie-menu")])
 
     await (message.edit_text if is_callback else message.reply_text)(
         lang.anime_text,
