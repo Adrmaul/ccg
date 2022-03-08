204f50646aa5d2b86e36f3807e6f8b447017926e
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-03-07 21:41:52 -0800

    FO

--- amime/modules/anime/TV-SHOW/menu kategori TV.py
+++ amime/modules/anime/TV-SHOW/menu kategori TV.py
@@ -7,8 +7,8 @@ from pyromod.helpers import ikb
 from amime.amime import Amime
 
 
-@Amime.on_message(filters.cmd(r"tvshow_menu$") & filters.private)
-@Amime.on_callback_query(filters.regex(r"^tvshow_menu$"))
+@Amime.on_message(filters.cmd(r"ktgr_tvshow-menu$") & filters.private)
+@Amime.on_callback_query(filters.regex(r"^ktgr_tvshow-menu$"))
 async def anime_menu(bot: Amime, union: Union[CallbackQuery, Message]):
     is_callback = isinstance(union, CallbackQuery)
     message = union.message if is_callback else union
@@ -16,13 +16,9 @@ async def anime_menu(bot: Amime, union: Union[CallbackQuery, Message]):
 
     keyboard = [
         [
-            (lang.TOP, "tvshow_top anime 1"),
-            (lang.TRENDING, "tvshow_trending anime 1"),
-            (lang.UPCOMING, "tvshow_upcoming anime 1"),
-        ],
-        [
-            (lang.KATEGORI, "ktgr_tv-menu"),
-        ],
+            (lang.FINISHED, "ktgr-finish anime 1"),
+            (lang.ONGOING, "ktgr-ongoing anime 1"),
+        ],    
     ]
 
     if is_callback:
