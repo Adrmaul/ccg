c7c8e07e3774e394a253cc26e5d13635ca1d508b
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-03-09 01:59:04 -0800

    f

--- amime/modules/anime/TV-SHOW/KATEGORI/SCIFI/ktgr_action10.py
+++ amime/modules/anime/TV-SHOW/KATEGORI/SCIFI/ktgr_action10.py
@@ -8,7 +8,7 @@ from pyromod.nav import Pagination
 from amime.amime import Amime
 
 
-@Amime.on_callback_query(filters.regex(r"^tv_scifi anime (?P<page>\d+)"))
+@Amime.on_callback_query(filters.regex(r"^tv_scifi10 anime (?P<page>\d+)"))
 async def anime_suggestions(bot: Amime, callback: CallbackQuery):
     page = int(callback.matches[0]["page"])
 
@@ -21,8 +21,8 @@ async def anime_suggestions(bot: Amime, callback: CallbackQuery):
             url="https://graphql.anilist.co",
             json=dict(
                 query="""
-                query($page: Int, $perPage: Int) {
-                    Page(page: $page, perPage: $perPage) {
+                query($per_page: Int) {
+                    Page(page: 11, perPage: $per_page) {
                         media(type: ANIME, format: TV, sort: TRENDING_DESC, status: FINISHED, genre: "sci-fi") {
                             id
                             title {
@@ -57,14 +57,14 @@ async def anime_suggestions(bot: Amime, callback: CallbackQuery):
                 suggestions,
                 item_data=lambda i, pg: f"menu {i.id}",
                 item_title=lambda i, pg: i.title.romaji,
-                page_data=lambda pg: f"tv_scifi anime {pg}",
+                page_data=lambda pg: f"tv_scifi10 anime {pg}",
             )
 
             lines = layout.create(page, lines=8)
 
             if len(lines) > 0:
                 keyboard += lines
-    keyboard.append([(lang.Next, "tv_scifi1 anime 1")])
+    keyboard.append([(lang.Prev, "tv_scifi9 anime 1"), (lang.Next, "tv_scifi11 anime 1")])
     keyboard.append([(lang.back_button, "ktgr-finish")])
 
     await message.edit_text(
