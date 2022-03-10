13e9fa3bb0f950401877a1dda2a2c1f97c8fceb0
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-03-09 20:34:55 -0800

    tes

--- amime/modules/anime/view.py
+++ amime/modules/anime/view.py
@@ -194,7 +194,7 @@ async def anime_view(bot: Amime, union: Union[CallbackQuery, Message]):
             buttons.append(
                     (
                         lang.Tes, 
-                        f"desc_{anime.id}_ANI_True_{user}"
+                        f"desc_{anime.id}_ANI_True_{user_id}"
                     ),
                 )
 
