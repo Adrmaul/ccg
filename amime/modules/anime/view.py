161cb296f9c3311dad7a7a7431eda2b20334b4a5
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-03-10 05:47:59 -0800

    tes

--- amime/modules/anime/view.py
+++ amime/modules/anime/view.py
@@ -131,7 +131,7 @@ async def anime_view(bot: Amime, union: Union[CallbackQuery, Message]):
         buttons = [
             (
                         lang.view_more_button,
-                        f"btn_{anime.id}_ANI_{str(auth)}_{user.id}"
+                        f"desc_{anime.id}_ANI_True_{user.id}"
                     )
         ]
          
