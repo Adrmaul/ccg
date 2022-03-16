3fbf873d5883e41578bcd44cbfd0eb3a2e6cdc16
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-03-16 19:35:16 +0700

    tes

--- amime/modules/anime/fitur.py
+++ amime/modules/anime/fitur.py
@@ -249,9 +249,9 @@ async def anime_view_more(bot: Amime, callback: CallbackQuery):
         anime = await client.get(anime_id, "anime")
 
         buttons = [
-            (lang.Info, f"infodownload"),
             (lang.Video, f"{anime.title.romaji} | video", "switch_inline_query_current_chat"),
             (lang.Audio, f"{anime.title.romaji} | audio", "switch_inline_query_current_chat"),
+            (lang.Info, f"infodownload"),
             #(lang.characters_button, f"anime characters {anime_id} {user_id}"),
         ]
 
