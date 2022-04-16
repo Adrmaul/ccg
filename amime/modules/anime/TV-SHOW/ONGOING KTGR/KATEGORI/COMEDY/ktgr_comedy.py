f447cdf56bb0180f1e79bf95c1a31bac99bb668d
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-04-16 14:35:33 +0700

    TES

--- amime/modules/anime/TV-SHOW/ONGOING KTGR/KATEGORI/COMEDY/ktgr_comedy.py
+++ amime/modules/anime/TV-SHOW/ONGOING KTGR/KATEGORI/COMEDY/ktgr_comedy.py
@@ -8,7 +8,7 @@ from pyromod.nav import Pagination
 from amime.amime import Amime
 
 
-@Amime.on_callback_query(filters.regex(r"^tv_ongoing_comedy1 anime (?P<page>\d+)"))
+@Amime.on_callback_query(filters.regex(r"^tv_ongoing_comedy anime (?P<page>\d+)"))
 async def anime_suggestions(bot: Amime, callback: CallbackQuery):
     page = int(callback.matches[0]["page"])
 
@@ -21,8 +21,8 @@ async def anime_suggestions(bot: Amime, callback: CallbackQuery):
             url="https://graphql.anilist.co",
             json=dict(
                 query="""
-                query($per_page: Int) {
-                    Page(page: 1, perPage: $per_page) {
+                query($page: Int, $perPage: Int) {
+                    Page(page: $page, perPage: $perPage) {
                         media(type: ANIME, format: TV, sort: TRENDING_DESC, status: RELEASING, genre: "comedy") {
                             id
                             title {
@@ -57,14 +57,14 @@ async def anime_suggestions(bot: Amime, callback: CallbackQuery):
                 suggestions,
                 item_data=lambda i, pg: f"menu {i.id}",
                 item_title=lambda i, pg: i.title.romaji,
-                page_data=lambda pg: f"tv_ongoing_comedy1 anime {pg}",
+                page_data=lambda pg: f"tv_ongoing_comedy anime {pg}",
             )
 
             lines = layout.create(page, lines=8)
 
             if len(lines) > 0:
                 keyboard += lines
-    keyboard.append([(lang.Prev, "tv_ongoing_comedy anime 1"), (lang.Next, "tv_ongoing_comedy2 anime 1")])
+    keyboard.append([(lang.Next, "tv_ongoing_comedy1 anime 1")])
     keyboard.append([(lang.back_button, "ktgr-ongoing")])
 
     await message.edit_text(
