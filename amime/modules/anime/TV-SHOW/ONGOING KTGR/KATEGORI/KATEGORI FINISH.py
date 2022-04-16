6d9da151860195bc2b66f062c672a1bfec2e4369
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-04-16 14:19:43 +0700

    tes

---
+++ amime/modules/anime/TV-SHOW/ONGOING KTGR/KATEGORI/KATEGORI FINISH.py
@@ -0,0 +1,62 @@
+from typing import Union
+
+from pyrogram import filters
+from pyrogram.types import CallbackQuery, Message
+from pyromod.helpers import ikb
+
+from amime.amime import Amime
+
+
+@Amime.on_message(filters.cmd(r"ktgr-ongoing$") & filters.private)
+@Amime.on_callback_query(filters.regex(r"^ktgr-ongoing$"))
+async def anime_menu(bot: Amime, union: Union[CallbackQuery, Message]):
+    is_callback = isinstance(union, CallbackQuery)
+    message = union.message if is_callback else union
+    lang = union._lang
+
+    keyboard = [
+        [
+            (lang.ACTION, "tv_ongoing_action anime 1"),
+            (lang.ADVENTURE, "tv_ongoing_adventure anime 1"),
+        ],
+        [
+            (lang.COMEDY, "tv_ongoing_comedy anime 1"),
+            (lang.DRAMA, "tv_ongoing_drama anime 1"),
+        ],
+        [
+            (lang.ECCHI, "tv_ongoing_ecchi anime 1"),
+            (lang.FANTASY, "tv_ongoing_fantasy anime 1"),
+        ],
+        [
+            (lang.HORROR, "tv_ongoing_horror anime 1"),
+            (lang.MAHOUSJ, "tv_ongoing_mahousj anime 1"),
+        ],
+        [
+            (lang.MECHA, "tv_ongoing_mecha anime 1"),
+            (lang.MUSIC, "tv_ongoing_music anime 1"),
+        ],
+        [
+            (lang.MYSTERY, "tv_ongoing_mystery anime 1"),
+            (lang.PSYCHOLOGICAL, "tv_ongoing_psychological anime 1"),
+        ],
+        [
+            (lang.ROMANCE, "tv_ongoing_romance anime 1"),
+            (lang.SCIFI, "tv_ongoing_scifi anime 1"),
+        ],
+        [
+            (lang.SOL, "tv_ongoing_sol anime 1"),
+            (lang.SPORTS, "tv_ongoing_sports anime 1"),
+        ],
+        [
+            (lang.SUPERNATURAL, "tv_ongoing_supernatural anime 1"),
+            (lang.THRILLER, "tv_ongoing_thriller anime 1"),
+        ],
+    ]
+
+    if is_callback:
+        keyboard.append([(lang.back_button, "ktgr_tvshow-menu")])
+
+    await (message.edit_text if is_callback else message.reply_text)(
+        lang.anime_text,
+        reply_markup=ikb(keyboard),
+    )
---
+++ amime/modules/anime/TV-SHOW/ONGOING KTGR/KATEGORI/SUPERNATURAL/ktgr_action8.py
@@ -0,0 +1,73 @@
+import httpx
+from anilist.types import Anime
+from pyrogram import filters
+from pyrogram.types import CallbackQuery
+from pyromod.helpers import ikb
+from pyromod.nav import Pagination
+
+from amime.amime import Amime
+
+
+@Amime.on_callback_query(filters.regex(r"^tv_supernatural8 anime (?P<page>\d+)"))
+async def anime_suggestions(bot: Amime, callback: CallbackQuery):
+    page = int(callback.matches[0]["page"])
+
+    message = callback.message
+    lang = callback._lang
+
+    keyboard = []
+    async with httpx.AsyncClient(http2=True) as client:
+        response = await client.post(
+            url="https://graphql.anilist.co",
+            json=dict(
+                query="""
+                query($per_page: Int) {
+                    Page(page: 9, perPage: $per_page) {
+                        media(type: ANIME, format: TV, sort: TRENDING_DESC, status: FINISHED, genre: "supernatural") {
+                            id
+                            title {
+                                romaji
+                                english
+                                native
+                            }
+                            siteUrl
+                        }
+                    }
+                }
+                """,
+                variables=dict(
+                    perPage=100,
+                ),
+            ),
+            headers={
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+        )
+        data = response.json()
+        await client.aclose()
+        if data["data"]:
+            items = data["data"]["Page"]["media"]
+            suggestions = [
+                Anime(id=item["id"], title=item["title"], url=item["siteUrl"])
+                for item in items
+            ]
+
+            layout = Pagination(
+                suggestions,
+                item_data=lambda i, pg: f"menu {i.id}",
+                item_title=lambda i, pg: i.title.romaji,
+                page_data=lambda pg: f"tv_supernatural8 anime {pg}",
+            )
+
+            lines = layout.create(page, lines=8)
+
+            if len(lines) > 0:
+                keyboard += lines
+    keyboard.append([(lang.Prev, "tv_supernatural7 anime 1"), (lang.Next, "tv_supernatural9 anime 1")])
+    keyboard.append([(lang.back_button, "ktgr-finish")])
+
+    await message.edit_text(
+        lang.suggestions_text,
+        reply_markup=ikb(keyboard),
+    )
