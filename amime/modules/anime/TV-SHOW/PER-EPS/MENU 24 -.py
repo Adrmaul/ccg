fcf22c9e917d9838cf5157718e32c6b1069a66be
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-04-19 13:06:22 +0700

    tes

--- amime/modules/anime/TV-SHOW/PER-EPS/MENU 24 -.py
+++ amime/modules/anime/TV-SHOW/PER-EPS/MENU 24 -.py
@@ -7,8 +7,8 @@ from pyromod.helpers import ikb
 from amime.amime import Amime
 
 
-@Amime.on_message(filters.cmd(r"ktgr-episode$") & filters.private)
-@Amime.on_callback_query(filters.regex(r"^ktgr-episode$"))
+@Amime.on_message(filters.cmd(r"ktgr-24-$") & filters.private)
+@Amime.on_callback_query(filters.regex(r"^ktgr-24-$"))
 async def anime_menu(bot: Amime, union: Union[CallbackQuery, Message]):
     is_callback = isinstance(union, CallbackQuery)
     message = union.message if is_callback else union
@@ -16,14 +16,14 @@ async def anime_menu(bot: Amime, union: Union[CallbackQuery, Message]):
 
     keyboard = [
         [
-            (lang.lebih24_button, "ktgr-24+"),
-            (lang.kurang24_button, "ktgr-24-"),
-
-        ],    
+            (lang.TOP, "tvshow_24-_top anime 1"),
+            (lang.TRENDING, "tvshow_24-_trending anime 1"),
+            (lang.UPCOMING, "tvshow_24-_upcoming anime 1"),
+        ],
     ]
 
     if is_callback:
-        keyboard.append([(lang.back_button, "ktgr_tvshow-menu")])
+        keyboard.append([(lang.back_button, "ktgr-episode")])
 
     await (message.edit_text if is_callback else message.reply_text)(
         lang.anime_text,
