abb42bc193e538a83ac4256eaf64b4e59396faa0
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-04-19 09:16:18 +0700

    tes

--- amime/modules/anime/TV-SHOW/ONGOING KTGR/KATEGORI/ACTION/ktgr_action.py
+++ amime/modules/anime/TV-SHOW/ONGOING KTGR/KATEGORI/ACTION/ktgr_action.py
@@ -23,7 +23,7 @@ async def anime_suggestions(bot: Amime, callback: CallbackQuery):
                 query="""
                 query($page: Int, $perPage: Int) {
                     Page(page: $page, perPage: $perPage) {
-                        media(type: ANIME, format: TV, sort: TRENDING_DESC, status: RELEASING, genre: "action", episodes: 12) {
+                        media(type: ANIME, format: TV, sort: TRENDING_DESC, status: RELEASING, genre: "action", episodes: 15^25) {
                             id
                             title {
                                 romaji
