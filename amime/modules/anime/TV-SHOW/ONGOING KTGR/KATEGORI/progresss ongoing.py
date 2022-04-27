6cf53c6838e9dd02d107a25d6e029c1d029aa69f
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-04-27 12:17:45 +0700

    te

--- amime/modules/anime/TV-SHOW/ONGOING KTGR/KATEGORI/progresss ongoing.py
+++ amime/modules/anime/TV-SHOW/ONGOING KTGR/KATEGORI/progresss ongoing.py
@@ -10,6 +10,12 @@ import httpx
 from anilist.types import Anime
 from pyromod.nav import Pagination
 
+from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
+from pyromod.helpers import array_chunk, ikb
+from amime.database import Episodes, Users
+from amime.modules.favorites import get_favorite_button
+from amime.modules.notify import get_notify_button
+
 
 
 @Amime.on_message(filters.cmd(r"ktgr_progress$") & filters.private)
@@ -19,32 +25,12 @@ async def anime_menu(bot: Amime, union: Union[CallbackQuery, Message]):
     message = union.message if is_callback else union
     lang = union._lang
 
-    keyboard = [
-        [
-            (lang.anime_satu, f"menu 140960" ),
-            (lang.anime_dua, f"menu 125367"),
-        ],
-        [
-            (lang.anime_tiga, f"menu 136080"),
-            (lang.anime_empat, f"menu 130586"),
-        ],
-        [
-            (lang.anime_lima, f"menu 125124"),
-            (lang.anime_enam, f"menu 116605"),
-        ],
-        [
-            (lang.anime_tujuh, f"menu 129201"),
-            (lang.anime_delapan, f"menu 111321"),
-        ],
-        #[
-            (lang.anime_sembilan, f"menu {i.id}"),
-            (lang.anime_sepuluh, f"menu {i.id}"),
-        #],
-        #[
-            (lang.aime11, "tv_ongoing_mystery anime 1"),
-            (lang.anime12, "tv_ongoing_psychological anime 1"),
-        #],
-    ]
+    buttons = [
+            (
+                        lang.anime_satu,
+                        f"menu 140960"
+                    )       
+        ]
 
     if is_callback:
         keyboard.append([(lang.back_button, "ktgr-ongoing")])
