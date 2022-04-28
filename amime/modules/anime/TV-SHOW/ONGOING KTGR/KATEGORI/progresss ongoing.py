a9f6457028af0198fb3327825cafc151b83195e4
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-04-28 21:11:21 +0700

    tes

--- amime/modules/anime/TV-SHOW/ONGOING KTGR/KATEGORI/progresss ongoing.py
+++ amime/modules/anime/TV-SHOW/ONGOING KTGR/KATEGORI/progresss ongoing.py
@@ -4,7 +4,16 @@ from pyrogram import filters
 from pyrogram.types import CallbackQuery, Message
 from pyromod.helpers import ikb
 
+import datetime
+import re
+import anilist
+from pyromod.helpers import array_chunk, ikb
+
 from amime.amime import Amime
+from amime.config import CHATS
+from amime.database import Requests
+from amime.modules.anime.view import anime_view
+
 
 
 
@@ -17,7 +26,7 @@ async def anime_menu(bot: Amime, union: Union[CallbackQuery, Message]):
 
     keyboard = [
         [
-            (lang.anime_satu, f"menu 140960" ),
+            (lang.anime_satu, f"menu 140960"),
             (lang.anime_dua, f"menu 125367"),
         ],
         [
