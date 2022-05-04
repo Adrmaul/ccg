e53f1295fae1eb197083c875a37bafca9b2382dd
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-05-04 08:11:35 +0700

    tes

--- amime/modules/anime/fitur.py
+++ amime/modules/anime/fitur.py
@@ -165,7 +165,7 @@ async def anime_view(bot: Amime, union: Union[CallbackQuery, Message]):
        #             f"manage anime {anime.id} 0 1 {language} 1",
        #         )
        #     )
-        if is_private and not anime.status.lower() == "releasing" and not anime.status.lower() == "not_yet_released" and not anime.genres() == "hentai": 
+        if is_private and not anime.status.lower() == "releasing" and not anime.status.lower() == "not_yet_released" and not anime.genres() == 'hentai': 
             buttons.append(
                     (
                         lang.batch_text_button,
@@ -190,7 +190,7 @@ async def anime_view(bot: Amime, union: Union[CallbackQuery, Message]):
             )   
              
 
-        if is_private and not anime.status.lower() == "not_yet_released" and not anime.genres() == "hentai":        
+        if is_private and not anime.status.lower() == "not_yet_released" and not anime.genres() == 'hentai':        
             buttons.append(
                     (
                         lang.Download_text, 
@@ -207,7 +207,7 @@ async def anime_view(bot: Amime, union: Union[CallbackQuery, Message]):
                 )         
 
 
-        if is_private and not anime.status.lower() == "not_yet_released" and not anime.genres() == "hentai": 
+        if is_private and not anime.status.lower() == "not_yet_released" and not anime.genres() == 'hentai': 
             button = (
                 lang.request_content_button,
                 f"request episodes {anime.id} {language}",
