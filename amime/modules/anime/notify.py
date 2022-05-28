a63818eed30de277f7996ee8f27daecfbe58efa0
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-05-29 04:00:05 +0700

    Update notify.py

--- amime/modules/anime/notify.py
+++ amime/modules/anime/notify.py
@@ -160,7 +160,6 @@ async def notify_episodes_confirm(bot: Amime, callback: CallbackQuery):
             reply_markup=ikb(keyboard),
         )
     
-     try:
         await bot.send_photo(
             CHANNELS["ani01"],
             f"https://img.anili.st/media/{anime.id}",
