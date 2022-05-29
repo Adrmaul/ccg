4df44c689edce9ac7c477db0c40f751511e97eb9
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-05-29 16:13:25 +0700

    tes

--- amime/modules/anime/view.py
+++ amime/modules/anime/view.py
@@ -226,7 +226,6 @@ async def anime_view(bot: Amime, union: Union[CallbackQuery, Message]):
             await (message.reply_photo)(
                 photo,
                 caption=text,
-                protect_content=True,
                 reply_markup=ikb(keyboard),
             )
 
