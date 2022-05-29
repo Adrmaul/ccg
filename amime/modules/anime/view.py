d8a19636b29c7775db2016ea7ad92d7dd7b0cd3f
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-05-29 17:51:56 +0700

    Update view.py

--- amime/modules/anime/view.py
+++ amime/modules/anime/view.py
@@ -136,17 +136,17 @@ async def anime_view(bot: Amime, union: Union[CallbackQuery, Message]):
         ]
          
 
-        if len(episodes) > 0:
-            if is_private and is_collaborator:
-                if anime.format.lower() == "movie":
-                    buttons.append((lang.watch_button, f"episode {anime.id} 0 1"))
+     #   if len(episodes) > 0:
+       #     if is_private and is_collaborator:
+         #       if anime.format.lower() == "movie":
+        #            buttons.append((lang.watch_button, f"episode {anime.id} 0 1"))
                 else:
-                    buttons.append(
+          #          buttons.append(
                        (
-                            lang.watch_button,
-                            f"episodes {anime.id} {episodes[0].season} 1",
-                        )
-                    )
+             #               lang.watch_button,
+              #              f"episodes {anime.id} {episodes[0].season} 1",
+                   #     )
+              #      )
 
 
 
