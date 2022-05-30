93d255503cca5f6f847b627f6c81543ad545451a
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-05-30 12:31:28 +0700

    tes

--- amime/modules/anime/view.py
+++ amime/modules/anime/view.py
@@ -32,7 +32,7 @@ from pyromod.helpers import array_chunk, ikb
 from amime.amime import Amime
 from amime.database import Episodes, Users
 from amime.modules.favorites import get_favorite_button
-from amime.modules.mylist import get_mylist_button
+from amime.modules.mylists import get_mylist_button
 from amime.modules.notify import get_notify_button
 
 
