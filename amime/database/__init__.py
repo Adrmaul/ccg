3c3a7bf92e61167ab36f1d7a74e3bf8bc1c8efff
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-05-31 09:41:23 +0700

    tes

--- amime/database/__init__.py
+++ amime/database/__init__.py
@@ -143,7 +143,7 @@ async def connect_database():
         {
             "connections": {
                 "bot_db": os.getenv(
-                    "DATABASE_URL", "postgresql://brctoxboaccntd:6e5ee4489d246cde117fc756ab1ac14748bbf4b830b58c80c295c78aea6992ff@containers-us-west-57.railway.app:5851/d54vm0k01b1ojg"
+                    "DATABASE_URL", "postgres://brctoxboaccntd:6e5ee4489d246cde117fc756ab1ac14748bbf4b830b58c80c295c78aea6992ff@containers-us-west-57.railway.app:5851/d54vm0k01b1ojg"
                 )
             },
             "apps": {"bot": {"models": [__name__], "default_connection": "bot_db"}},
