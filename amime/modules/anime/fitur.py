3fdbefc5288a8a9d7d0b9563aa179055179cecd4
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-06-01 20:27:00 +0700

    tes

--- amime/modules/anime/fitur.py
+++ amime/modules/anime/fitur.py
@@ -250,19 +250,16 @@ async def anime_view_more(bot: Amime, callback: CallbackQuery):
     async with anilist.AsyncClient() as client:
         anime = await client.get(anime_id, "anime")
 
+
         if hasattr(anime, "trailer"):
             if hasattr(anime.trailer, "url"):
-                buttons.append((lang.trailer_button, anime.trailer.url, "url")),
+                buttons.append((lang.trailer_button, anime.trailer.url, "url"))
 
-        buttons.append([lang.characters_button, f"anime characters {anime.id} {user_id}"]),
-
-        
         buttons = [
             (lang.Video, f"{anime.title.romaji} | video", "switch_inline_query_current_chat"),
             (lang.Audio, f"{anime.title.romaji} | audio", "switch_inline_query_current_chat"),
-            
-            
-            #(lang.characters_button, f"anime characters {anime_id} {user_id}"),
+        ],[
+            (lang.characters_button, f"anime characters {anime_id} {user_id}"),
         ]
 
         
@@ -343,7 +340,7 @@ async def anime_view_characters(bot: Amime, callback: CallbackQuery):
 
         keyboard = [
             [
-                (lang.back_button, f"download more {anime_id} {user_id}"),
+                (lang.back_button, f"anime more {anime_id} {user_id}"),
             ],
         ]
 
