785e96bcd5f02204530f5dc82b1147e6257bcf2e
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-06-12 16:02:22 +0700

    tes

---
+++ amime/modules/anime/TV-SHOW/SEASON/Musim/2014 ANIME/MENU MUSIM.py
@@ -0,0 +1,37 @@
+from typing import Union
+
+from pyrogram import filters
+from pyrogram.types import CallbackQuery, Message
+from pyromod.helpers import ikb
+
+from amime.amime import Amime
+
+
+@Amime.on_message(filters.cmd(r"2014_se$") & filters.private)
+@Amime.on_callback_query(filters.regex(r"^2014_se$"))
+async def anime_menu(bot: Amime, union: Union[CallbackQuery, Message]):
+    is_callback = isinstance(union, CallbackQuery)
+    message = union.message if is_callback else union
+    lang = union._lang
+
+    keyboard = [
+        [
+            (lang.musim_salju, "winter_2014 anime 1"),
+            (lang.musim_gugur, "spring_2014 anime 1"),
+
+        ],
+        [
+            (lang.musim_panas, "summer_2014 anime 1"),
+            (lang.musim_semi, "fall_2014 anime 1"),
+
+        ],   
+    ]
+
+    if is_callback:
+        keyboard.append([(lang.back_button, "ktgr-season")])
+
+    await (message.edit_text if is_callback else message.reply_text)(
+        lang.musim_text,
+        reply_markup=ikb(keyboard),
+    )
+
