132bcc0f4b0a0ed7cfa8c45830e53edc237ab26d
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-06-13 18:32:49 +0700

    tes

--- amime/modules/anime/watch.py
+++ amime/modules/anime/watch.py
@@ -72,12 +72,10 @@ async def anime_episode(bot: Amime, callback: CallbackQuery):
         if episode is not None:
             await Viewed.get_or_create(user=user.id, item=episode.id, type="anime")
 
-            keyboard = [[await get_watched_button(lang, user, episode.id)], [
-                    (
+            keyboard = [[await get_watched_button(lang, user, episode.id), (
                         lang.report_button,
                         f"report episode {anime_id} {season} {number} -1",
-                    )
-                ]]
+                    )]]
 
             if len(episode.name) > 0:
                 text += f"\n<b>{lang.name}</b>: <code>{episode.name}</code>"
