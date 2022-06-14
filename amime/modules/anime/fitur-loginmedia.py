3450e38ade84f030c913723ac9ea9ad22d57f607
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-06-14 19:07:17 +0700

    tes

--- amime/modules/anime/fitur-loginmedia.py
+++ amime/modules/anime/fitur-loginmedia.py
@@ -52,6 +52,22 @@ async def anime_episodes(bot: Amime, callback: CallbackQuery):
         ),
     ]
 
+    if len(episodes) > 0 and not anime.status.lower() == "not_yet_released":
+            button = (
+                lang.request_content_button,
+                f"request episodes {anime.id} {language}",
+            )
+            if anime.status.lower() == "releasing":
+                if hasattr(anime, "next_airing"):
+                    next_episode = anime.next_airing.episode
+                    if len(episodes) < (next_episode - 1):
+                        buttons.append(button)
+                else:
+                    buttons.append(button)
+            elif hasattr(anime, "episodes"):
+                if len(episodes) < anime.episodes:
+                    buttons.append(button)
+
     if season > 0:
         buttons.append(
             (
@@ -115,10 +131,7 @@ async def anime_episodes(bot: Amime, callback: CallbackQuery):
     if len(lines) > 0:
         keyboard += lines
 
-    keyboard.append([(
-                        lang.Download_text, 
-                        f"download more {anime_id} {user.id}"
-                    ), (lang.back_button, f"btn_{anime_id}_True_{user.id}")])
+    keyboard.append([(lang.inline, f"{anime.title.romaji}", "switch_inline_query_current_chat"), (lang.back_button, f"btn_{anime_id}_True_{user.id}")])
 
     await callback.edit_message_media(
         InputMediaPhoto(
