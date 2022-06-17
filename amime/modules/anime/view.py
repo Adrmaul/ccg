86781c711fef7e6c99425c2e90e7b9e974079787
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-06-17 09:43:34 +0700

    tes

--- amime/modules/anime/view.py
+++ amime/modules/anime/view.py
@@ -157,7 +157,7 @@ async def anime_view(bot: Amime, union: Union[CallbackQuery, Message]):
 
 
 
-        if is_private and is_collaborator:
+        if is_collaborator:
             buttons.append(
                 (
                     lang.manage_button,
