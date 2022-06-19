0b111447aba59c5f75d8d629c5dad7acc6b34212
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-06-19 17:40:56 +0700

    yrd

--- amime/modules/anime/Anime-media-login/settings_login.py
+++ amime/modules/anime/Anime-media-login/settings_login.py
@@ -126,7 +126,7 @@ async def anime_view(bot: Amime, union: Union[CallbackQuery, Message]):
                 )
             )
 
-        if is_auth and not anime.status.lower() == "not_yet_released":
+        if is_auth and is_collaborator and not anime.status.lower() == "not_yet_released":
             buttons.append(
                 (
                     lang.manage_user_button,
