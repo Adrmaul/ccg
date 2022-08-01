98f578c5f7b383e6db31ae8431d41070d0504f55
Adrmaul <89143405+Adrmaul@users.noreply.github.com>
2022-08-01 21:12:43 +0700

    Update config.py

--- amime/config.py
+++ amime/config.py
@@ -21,7 +21,7 @@ CHATS: Dict = {
     "chanireborn": -1001763186709,
 }
 
-COLLABORATOR_USERS: List[int] = [1891259266, 854915330, 1765145725, 1929973545, 1422633582, 1763576099, 1822182140, 1757259822, 992332019, 1675401159] # @satyo @yaneka @liveuser @chihi @Xeno @Bell, @Brian, @masdihan, @razan, @Yusuf
-SUDO_USERS: List[int] = [784985038, 1177818563, 1457083910, ]  # @yudi, @adrian and @anggi
+COLLABORATOR_USERS: List[int] = [1891259266, 854915330, 1765145725, 1929973545, 1422633582, 1763576099, 1822182140, 1757259822, 992332019, 1675401159, 616954243, 1284373383, 1696268688] # @satyo @yaneka @liveuser @chihi @Xeno @Bell, @Brian, @masdihan, @razan, @Yusuf
+SUDO_USERS: List[int] = [784985038, 1177818563, 1457083910, ]  # @yudi, @adrian and @anggi, 
 PREFIXES: List[str] = ["!", "/"] # Like !start or /start
 #tes
