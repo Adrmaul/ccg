d72c108bd7130cb7f4ca7432c3d5bf85045db3ef
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-08-14 13:01:00 +0700

    tws

--- amime/modules/anime/view.py
+++ amime/modules/anime/view.py
@@ -134,7 +134,7 @@ async def anime_view(bot: Amime, union: Union[CallbackQuery, Message]):
                 nextAir = {anime.next_airing.time_until}
                 air_on = make_it_rw(nextAir*1000)
                 text = f"✅ List Episode Tersedia untuk ditonton. - <code>{anime.title.romaji}"
-                if hasattr(anime.next_airing, "time_until"):
+                if hasattr(anime.next_airing, "time_until") and air_on:
                     text += f"\nEpisode ({anime.next_airing.episode}) akan rilis dalam {air_on}"
             if len(episodes) < 1 :
                 text = f"\n\n❌ belum tersedia. - <code>{anime.title.romaji}"
