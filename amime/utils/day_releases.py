e822437c3cd3512c92ccdef721ed573d1944da1d
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-09-01 20:44:04 +0700

    tes

--- amime/utils/day_releases.py
+++ amime/utils/day_releases.py
@@ -58,7 +58,7 @@ async def load(bot):
                     number = anime.next_airing.episode
                     date = datetime.datetime.fromtimestamp(
                         anime.next_airing.at
-                    ).datetime.now(tz=tz)
+                    ).replace(tzinfo=datetime.datetime.now(tz=tz))
 
                     if date.day == now.day:
                         animes[anime_id] = [
