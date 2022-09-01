850b229877e254ae14f9c1ba7fbbbd6098d3d0d4
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-09-01 20:17:59 +0700

    tes

--- amime/utils/day_releases.py
+++ amime/utils/day_releases.py
@@ -29,11 +29,9 @@ import anilist
 from amime.config import CHATS
 from amime.database import Episodes
 
-IST = pytz.timezone('Asia/Kolkata')
 
 async def load(bot):
-    now = datetime.datetime.now().replace(datetime.now(IST))
-    
+    now = datetime.datetime.now().astimezone(timezone('Asia/jakarta'))
 
     sent = await bot.send_message(CHATS["staff"], "Checking the day's releases...")
 
