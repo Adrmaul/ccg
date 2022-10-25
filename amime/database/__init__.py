971fdc8d2e9957196827824f45a7494afe305697
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-10-25 21:10:59 +0700

    Update __init__.py

--- amime/database/__init__.py
+++ amime/database/__init__.py
@@ -144,7 +144,7 @@ async def connect_database():
         {
             "connections": {
                 "bot_db": os.getenv(
-                    "DATABASE_URL", "postgres://amime/database/ccgnimex.sql"
+                    "DATABASE_URL", "postgres://admin:cinangka3@188.166.241.229:3291/ccgnimex"
                 )
             },
             "apps": {"bot": {"models": [__name__], "default_connection": "bot_db"}},
