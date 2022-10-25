a292a017d7298bc63b74867d66542ac651e35c4c
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-10-25 19:55:23 +0700

    Update __init__.py

--- amime/database/__init__.py
+++ amime/database/__init__.py
@@ -144,7 +144,7 @@ async def connect_database():
         {
             "connections": {
                 "bot_db": os.getenv(
-                    "DATABASE_URL", "postgres://admin:cinangka3@188.166.241.229:3291/ccgnimex"
+                    "DATABASE_URL", "sqlite://amime/database/sqlitedb.sqlite"
                 )
             },
             "apps": {"bot": {"models": [__name__], "default_connection": "bot_db"}},
