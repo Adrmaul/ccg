50d099622a54fa0c0577093807493d3bc9935503
akuiiki <43848105+Myudi422@users.noreply.github.com>
2023-03-14 23:18:38 +0700

    tes

--- amime/modules/anime/view.py
+++ amime/modules/anime/view.py
@@ -205,7 +205,7 @@ async def anime_view(bot: Amime, union: Union[CallbackQuery, Message]):
             buttons.append(await get_favorite_button(lang, user, "anime", anime.id))
         
         if is_private:
-            buttons.append(await get_mylist_button(lang, "anime", anime.id))
+            buttons.append(await get_mylist_button(lang, user.id, "anime", anime.id))
         
         if is_private:
             buttons.append(
