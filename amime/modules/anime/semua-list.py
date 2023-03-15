4c7d3541c7f5328f46132bb5cff07e8c2d19a7f3
akuiiki <43848105+Myudi422@users.noreply.github.com>
2023-03-15 21:32:03 +0700

    TES

--- amime/modules/anime/semua-list.py
+++ amime/modules/anime/semua-list.py
@@ -19,8 +19,40 @@ async def anime_menu(bot: Amime, union: Union[CallbackQuery, Message]):
             (lang.a_button, "a_lists anime 1"),
             (lang.b_button, "b_lists anime 1"),
             (lang.c_button, "c_lists anime 1"),
-            (lang.d_button, "a_lists anime 1"),
-            (lang.e_button, "b_lists anime 1"),
+            (lang.d_button, "d_lists anime 1"),
+            (lang.e_button, "e_lists anime 1"),
+        ],
+        [
+            (lang.f_button, "f_lists anime 1"),
+            (lang.g_button, "g_lists anime 1"),
+            (lang.h_button, "h_lists anime 1"),
+            (lang.i_button, "i_lists anime 1"),
+            (lang.j_button, "j_lists anime 1"),
+        ],
+        [
+            (lang.k_button, "k_lists anime 1"),
+            (lang.l_button, "l_lists anime 1"),
+            (lang.m_button, "m_lists anime 1"),
+            (lang.n_button, "n_lists anime 1"),
+            (lang.o_button, "o_lists anime 1"),
+        ],
+        [
+            (lang.p_button, "p_lists anime 1"),
+            (lang.q_button, "q_lists anime 1"),
+            (lang.r_button, "r_lists anime 1"),
+            (lang.s_button, "s_lists anime 1"),
+            (lang.t_button, "t_lists anime 1"),
+        ],
+        [
+            (lang.u_button, "u_lists anime 1"),
+            (lang.v_button, "v_lists anime 1"),
+            (lang.w_button, "w_lists anime 1"),
+            (lang.x_button, "x_lists anime 1"),
+            (lang.y_button, "y_lists anime 1"),
+        ],
+        [
+            (lang.Z_button, "Z_lists anime 1"),
+            (lang.0_button, "0_lists anime 1"),
         ],
     ]
 
