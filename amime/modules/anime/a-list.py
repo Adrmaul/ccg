f0efe4e97c33e99d3f9fedf1d750c0bfd5d193ee
akuiiki <43848105+Myudi422@users.noreply.github.com>
2023-03-16 15:50:24 +0700

    tes

--- amime/modules/anime/a-list.py
+++ amime/modules/anime/a-list.py
@@ -40,7 +40,7 @@ async def anime_a_lists(bot: Amime, callback: CallbackQuery):
 
     keyboard = []
     async with anilist.AsyncClient() as client:
-        a_lists = await A_lists.filter(type="anime").prefetch_related("anime")
+        a_lists = await A_lists.filter(type="anime")
 
         results = []
         for a_list in a_lists:
