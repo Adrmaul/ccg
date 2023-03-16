14151dcd7ff62d4557a9bce11ae80bd3a05209aa
akuiiki <43848105+Myudi422@users.noreply.github.com>
2023-03-16 18:56:07 +0700

    tes

--- amime/modules/anime/a-list.py
+++ amime/modules/anime/a-list.py
@@ -25,36 +25,31 @@ from pyrogram import filters
 from pyrogram.types import CallbackQuery
 from pyromod.helpers import ikb
 from pyromod.nav import Pagination
-import asyncio
 
 from amime.amime import Amime
 from amime.database import A_lists
-from asyncio import gather
 
 
+@Amime.on_callback_query(filters.regex(r"a_lists anime (?P<page>\d+)"))
+async def anime_a_lists(bot: Amime, callback: CallbackQuery):
+    page = int(callback.matches[0]["page"])
+    message = callback.message
+    user = callback.from_user
+    lang = callback._lang
 
-async with anilist.AsyncClient() as client:
-    a_lists = await A_lists.filter(type="anime")
-    total_data = len(a_lists)
-    total_pages = (total_data + 7) // 8  # Menentukan jumlah halaman dengan 8 item per halaman
-    offset = (page - 1) * 8
-    a_lists = a_lists.order_by("-created_at").limit(8, offset=offset)
+    async with anilist.AsyncClient() as client:
+        a_lists = await A_lists.filter(type="anime").limit(8).offset((page - 1) * 8)
+        anime_list = await asyncio.gather(*[client.get(a_list.item, "anime") for a_list in a_lists])
+        results = list(zip(a_lists, anime_list))
 
-    anime_list = await asyncio.gather(
-        *[client.get(a_list.item, "anime") for a_list in a_lists]
-    )
+        layout = Pagination(
+            results,
+            item_data=lambda i, pg: f"menu {i[0].item}",
+            item_title=lambda i, pg: i[1].title.romaji,
+            page_data=lambda pg: f"a_lists anime {pg}",
+        )
+        keyboard = layout.create(page, lines=8)
 
-    results = list(zip(a_lists, anime_list))
+    keyboard.append([(lang.back_button, "listsx")])
 
-    layout = Pagination(
-        results,
-        item_data=lambda i, pg: f"menu {i[0].item}",
-        item_title=lambda i, pg: i[1].title.romaji,
-        page_data=lambda pg: f"a_lists anime {pg}",
-    )
-
-    keyboard = layout.create(page, lines=8)
-
-keyboard.append([(lang.back_button, "listsx")])
-
-await message.edit_text(lang.mylist_text, reply_markup=ikb(keyboard))
+    await message.edit_text(lang.mylist_text, reply_markup=ikb(keyboard))
