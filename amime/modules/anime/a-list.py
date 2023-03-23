f83672db052443cc3adc1dbbbacdd27c43a36dff
akuiiki <43848105+Myudi422@users.noreply.github.com>
2023-03-23 14:46:31 +0700

    tes

--- amime/modules/anime/a-list.py
+++ amime/modules/anime/a-list.py
@@ -1,25 +1,3 @@
-# MIT License
-#
-# Copyright (c) 2021 Andriel Rodrigues for Amano Team
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 import anilist
 from pyrogram import filters
 from pyrogram.types import CallbackQuery
@@ -30,6 +8,9 @@ from amime.amime import Amime
 from amime.database import A_lists
 
 
+RESULTS_PER_PAGE = 8
+
+
 @Amime.on_callback_query(filters.regex(r"a_lists anime (?P<page>\d+)"))
 async def anime_a_lists(bot: Amime, callback: CallbackQuery):
     page = int(callback.matches[0]["page"])
@@ -38,32 +19,52 @@ async def anime_a_lists(bot: Amime, callback: CallbackQuery):
     user = callback.from_user
     lang = callback._lang
 
-    keyboard = []
     async with anilist.AsyncClient() as client:
         a_lists = await A_lists.filter(type="anime")
+        total_results = len(a_lists)
 
+        # Menghitung jumlah halaman yang tersedia
+        total_pages = (total_results - 1) // RESULTS_PER_PAGE + 1
+
+        # Mengambil hanya data yang dibutuhkan pada halaman yang diminta
+        start_index = (page - 1) * RESULTS_PER_PAGE
+        end_index = start_index + RESULTS_PER_PAGE
+        page_results = a_lists[start_index:end_index]
+
+        # Memuat data anime untuk setiap item di halaman
         results = []
-        for a_list in a_lists:
+        for a_list in page_results:
             anime = await client.get(a_list.item, "anime")
             results.append((a_list, anime))
 
+        # Membuat tampilan halaman dengan teknik pagination
         layout = Pagination(
             results,
             item_data=lambda i, pg: f"menu {i[0].item}",
             item_title=lambda i, pg: i[1].title.romaji,
             page_data=lambda pg: f"a_lists anime {pg}",
+            results_per_page=RESULTS_PER_PAGE,
+            total_results=total_results,
+            current_page=page,
+            total_pages=total_pages,
         )
 
-        lines = layout.create(page, lines=8)
+        # Menampilkan halaman saat ini
+        lines = layout.create(page, lines=RESULTS_PER_PAGE)
 
-        if len(lines) > 0:
-            keyboard += lines
+    # Menambahkan tombol navigasi ke halaman sebelumnya dan selanjutnya
+    keyboard = []
+    if page > 1:
+        keyboard.append([(lang.previous_button, f"a_lists anime {page-1}")])
+    if page < total_pages:
+        keyboard.append([(lang.next_button, f"a_lists anime {page+1}")])
 
+    # Menambahkan tombol kembali ke menu utama
     keyboard.append([(lang.back_button, "listsx")])
 
     # Mengirim jawaban callback query dengan menampilkan hasil
     await callback.answer()
     await message.edit_text(
         lang.mylist_text,
-        reply_markup=ikb(keyboard),
+        reply_markup=ikb(keyboard + lines),
     )
