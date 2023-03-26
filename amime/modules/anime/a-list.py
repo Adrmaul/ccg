80a78e35e430032abe59daa9d09229d0a35d2f28
akuiiki <43848105+Myudi422@users.noreply.github.com>
2023-03-26 11:26:43 +0700

    tes

--- amime/modules/anime/a-list.py
+++ amime/modules/anime/a-list.py
@@ -20,14 +20,28 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import asyncio
 import anilist
 from pyrogram import filters
 from pyrogram.types import CallbackQuery
 from pyromod.helpers import ikb
 from pyromod.nav import Pagination
+from aiocache import cached, SimpleMemoryCache
 
 from amime.amime import Amime
 from amime.database import A_lists
+
+
+async def fetch_anime_data(anime_ids):
+    async with anilist.AsyncClient() as client:
+        tasks = [client.get(anime_id, "anime") for anime_id in anime_ids]
+        return await asyncio.gather(*tasks)
+
+
+@cached(ttl=3600, cache=SimpleMemoryCache, key="a_lists_cache")
+async def get_a_lists():
+    a_lists = await A_lists.filter(type="anime")
+    return a_lists
 
 
 @Amime.on_callback_query(filters.regex(r"a_lists anime (?P<page>\d+)"))
@@ -39,31 +53,28 @@ async def anime_a_lists(bot: Amime, callback: CallbackQuery):
     lang = callback._lang
 
     keyboard = []
-    async with anilist.AsyncClient() as client:
-        a_lists = await A_lists.filter(type="anime")
 
-        results = []
-        for a_list in a_lists:
-            anime = await client.get(a_list.item, "anime")
-            results.append((a_list, anime))
+    a_lists = await get_a_lists()
+    anime_ids = [a_list.item for a_list in a_lists]
+    anime_data = await fetch_anime_data(anime_ids)
 
-        layout = Pagination(
-            results,
-            item_data=lambda i, pg: f"menu {i[0].item}",
-            item_title=lambda i, pg: i[1].title.romaji,
-            page_data=lambda pg: f"a_lists anime {pg}",
-        )
+    results = list(zip(a_lists, anime_data))
 
-        lines = layout.create(page, lines=8)
+    layout = Pagination(
+        results,
+        item_data=lambda i, pg: f"menu {i[0].item}",
+        item_title=lambda i, pg: i[1].title.romaji,
+        page_data=lambda pg: f"a_lists anime {pg}",
+    )
 
-        if len(lines) > 0:
-            keyboard += lines
+    lines = layout.create(page, lines=8)
+
+    if len(lines) > 0:
+        keyboard += lines
 
     keyboard.append([(lang.back_button, "listsx")])
 
-    # Mengirim jawaban callback query dengan menampilkan hasil
-    await callback.answer()
     await message.edit_text(
-        lang.mylist_text,
+        lang.a_lists_list_title,
         reply_markup=ikb(keyboard),
     )
